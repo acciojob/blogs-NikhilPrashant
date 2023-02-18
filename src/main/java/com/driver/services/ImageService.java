cc31b609738e46609f4406ff847d17eb203050c3
nikhilprashant <nikhilprashant1998@gmail.com>
2023-02-18 16:14:25 +0530

    Working Incomplete DB Dependencies

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -1,11 +1,10 @@
 package com.driver.services;
 
 import com.driver.models.*;
+import com.driver.models.Image;
 import com.driver.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ImageService {
@@ -17,15 +16,23 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog = blogRepository2.findById(blogId).get();
+        com.driver.models.Image image = new com.driver.models.Image(description, dimensions, blog);
+        imageRepository2.save(image);
+        blog.getImages().add(image);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-
+        Image image = imageRepository2.findById(id).get();
+        String[] imageDmns = image.getDimensions().split("X");
+        String[] screenDmns = screenDimensions.split("X");
+        int noOfImagesPossible = (Integer.parseInt(screenDmns[0]) / Integer.parseInt(imageDmns[0])) * (Integer.parseInt(screenDmns[1]) / Integer.parseInt(imageDmns[1]));
+        return noOfImagesPossible;
     }
 }
